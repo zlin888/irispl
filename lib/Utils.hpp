ca98168bfddbddd91d53beb7ce8855c9d74dc97f
‘zlin888’ <zhitaolin@outlook.com>
2020-02-08 15:13:22 +0800

    [dev] instruction constructor

--- lib/Utils.hpp
+++ lib/Utils.hpp
@@ -12,7 +12,6 @@ namespace utils {
         auto wsback=std::find_if_not(s.rbegin(),s.rend(),[](int c){return std::isspace(c);}).base();
         return (wsback<=wsfront ? std::string() : std::string(wsfront,wsback));
     }
-
 }
 
 #endif //TYPED_SCHEME_UTILS_HPP
